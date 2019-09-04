a0531ec10c33b71c369ab6f96390141500bb5353
Courtney Walker <cwalked@gmail.com>
2019-09-04 15:21:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,9 +78,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    :yukihiro_matsumoto => {
- 		    :known_for => "Ruby",
- 		    :languages => ["LISP", "C"]
+   programmer_hash
     
 end
 
